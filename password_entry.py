d464504002c19c25d3a73354986339c7696bcfef
BayauaAivan <aivan.bayaua@my.jcu.edu.au>
2019-08-22 12:53:57 +1000

    added password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,9 @@
 """Aivan"""
+
+MIN_LENGTH = 4
+
+password = input("Please enter a password of at least {} characters: ".format(MIN_LENGTH))
+while len(password) < MIN_LENGTH:
+    password = input("Please enter a password of at least {} characters: ".format(MIN_LENGTH))
+
+print("*" * len(password))
